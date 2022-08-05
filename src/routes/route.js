161f9864de0a4656906eb75c203cdc09bf450e0f
ajit-07 <sajit1619@gmail.com>
2022-08-05 19:43:33 +0530

    modified files commmited

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const abc = require('../introduction/intro')
+const wel = require('../logger/logger.js')
+const dat=require('../util/helper.js')
+const format=require('../validator/formatter.js')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
+    wel.welcomeFunction()
+    dat.todayDate()
+    dat.todayMonth()
+    dat.batchInfo()
+    format.trimedString()
+    format.lowercase()
+    format.uppercase()
     res.send('My second ever api!')
 });
 
