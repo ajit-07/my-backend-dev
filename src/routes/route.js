82e0fb8e714538d1f80b257fa5e680d971c59b10
ajit-07 <sajit1619@gmail.com>
2022-08-11 01:40:55 +0530

    commited post api assignments

--- src/routes/route.js
+++ src/routes/route.js
@@ -68,6 +68,92 @@ router.post("/test-post-4", function(req, res) {
     let ele= req.body.element
     arr.push(ele)
     res.send(  { msg: arr , status: true }  )
+    console.log("The pushed element inside the array is",ele)
+    console.log("The new array is",arr)
 })
+let players=[
+    {
+        "name":"manish",
+        "dob":"1/1/1995",
+        "gender":"male",
+        "city":"jalandhar",
+        "sports":["swiming"]
+    },
+    {
+        "name":"gopal",
+        "dob":"1/9/1995",
+        "gender":"male",
+        "city":"delhi",
+        "sports":["soccer"]
+    },
+    {
+        "name":"lokesh",
+        "dob":"1/1/1990",
+        "gender":"male",
+        "city":"mumbai",
+        "sports":["soccer"]
+    }
+
+]
+router.post('/players',function(req,res){
+    let newPlayer=req.body;
+    let newPlayerName=newPlayer.name;
+    let flag=false;
+    for(let i=0;i<players.length;i++){
+        if(players[i].name===newPlayerName){
+            flag=true;
+            break;
+        }
+    }
+    if (flag){
+        console.log("This player is already present in the array!!")
+        res.send("This player is already present in the array!!")
+    }else{
+        players.push(newPlayer)
+        console.log("After adding the new player the array is==>",players)
+        res.send(players)
+    }
+})
+let persons=[
+    {
+        "name":"Rohit Singh",
+        "age":10,
+        "votingstatus":false
+    },
+    {
+        "name":"Rahul Lokesh",
+        "age":20,
+        "votingstatus":false
+    },
+    {
+        "name":"Mahendra Singh Dhoni",
+        "age":70,
+        "votingstatus":false
+    },
+    {
+        "name":"Subha Mohanty",
+        "age":5,
+        "votingstatus":false
+    },
+    {
+        "name":"Hogward Hell",
+        "age":40,
+        "votingstatus":false
+    }
+]
+router.post('/voting-verification',function(req,res){
+    let votingAge=req.query.votingAge;
+    let eligiblePersonToVote=[];
+    for(let i=0;i<persons.length;i++){
+        if( persons[i].age>=votingAge){
+            persons[i].votingstatus=true;
+            eligiblePersonToVote.push(persons[i])
+        }
+    }
+    console.log("Eligible persons to vote==>",eligiblePersonToVote)
+    res.send(eligiblePersonToVote)
+})
+
+
 
 module.exports = router;
