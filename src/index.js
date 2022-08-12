5c7ac6af8e897799a839522468931ddafb44cb5b
ajit-07 <sajit1619@gmail.com>
2022-08-12 23:48:04 +0530

    schema code commited

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const {default: mongoose }=require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://ajit-07:pzD85GscINrNEeKB@cluster0.mzumpor.mongodb.net/ajit070698-DB"
+,{
+    useNewUrlParser: true
+}
+).then( () => {console.log( "Mongodb is connected")})
+.catch(err => console.log(err))
+
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
